a1aaac82e377f5a8ac395fa2e041e03c9a5dbb0c
Alexis Gonzalez <alexis_gonzalez@hotmail.fr>
2022-08-17 19:44:15 +0200

    Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants, except: [:edit, :destroy, :update] do
+    resources :reviews, only: [:create]
+  end
 end
